f1d7f963eb0ee38f9b0d7d6af1fd208d0976fd44
AceroJearmaine <jhemacero22@gmail.com>
2024-10-04 09:48:36 +0800

    Add files via upload

---
+++ 02BisicOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	cout << "Our Lady of Lourdes College foundation \n";
+	cout << "College of Computer Studies\n";
+	cout << "bachelor of science and Information Technology\n";
+	cout << "Jearmaine Winluv Acero\n";
+	cout << "p1 tulay na lupa labo camarines norte\n";
+	
+	return 0;
+	
+}
+	
---
+++ 04BasicInput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	string name;
+	int Course, School;
+	cout << "Nickname:";
+	cin >> name; 
+	cout << "Course-Year Level:";
+	cin >> Course;
+	cout << "School:";
+	cin >> School;
+	
+	return 0;
+
+}
+	
